dbb0cd150d1b63427e8c91f474ec80118fe4c6ab
Arun Kumar <arunmadcoder@gmail.com>
2021-05-16 09:31:32 +0530

    Added the new styles

--- src/app/movielist/movielist.component.ts
+++ src/app/movielist/movielist.component.ts
@@ -11,14 +11,17 @@ movies : any[];
   	this.movies = [{
   		name: "Matrix",
   		image_path: "assets/images/3.jpg",
+		desc: "At the Oracle's behest, Neo attempts to rescue the Keymaker and realises that to save Zion within 72 hours, he must confront the Architect. Meanwhile, Zion prepares for war against the machines.",
   		id: 1
   	},{
   		name: "Gravity",
   		image_path: "assets/images/2.jpeg",
+		desc: "On an outer space mission, Dr Ryan Stone, an engineer, and Matt Kowalski, an astronaut, are hit by high-speed space debris. As the situation gets dire, Stone, the lone survivor, rises to the occasion.",
   		id: 2
   	},{
   		name: "Interstellar",
   		image_path: "assets/images/1.jpg",
+		desc: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
   		id: 3
   	}];
    }
